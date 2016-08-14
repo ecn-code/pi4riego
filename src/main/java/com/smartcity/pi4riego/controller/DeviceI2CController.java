41912b3b4dd9c3a5201ee392e673cd280dc98442
elcanie <elcanie@inf.upv.es>
2016-08-14 20:03:12 +0200

    Descubrir I2C devices

--- src/main/java/com/smartcity/pi4riego/controller/DeviceI2CController.java
+++ src/main/java/com/smartcity/pi4riego/controller/DeviceI2CController.java
@@ -1,14 +1,10 @@
 package com.smartcity.pi4riego.controller;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 import com.smartcity.pi4riego.ApplicationStartup;
 import com.smartcity.pi4riego.entity.DeviceI2C;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -29,29 +25,25 @@ public class DeviceI2CController {
         i2cDevice.write(action.getBytes());
     }
 
-    public static ArrayList<Integer> discoverThings() throws IOException, I2CFactory.UnsupportedBusNumberException, ParseException {
+    public static ArrayList<Integer> discoverThings() throws IOException, I2CFactory.UnsupportedBusNumberException {
         ArrayList<Integer> things = new ArrayList<Integer>();
         I2CBus i2c = I2CFactory.getInstance(I2CBus.BUS_1);
-        byte[] buffer = new byte[100];
         for(int i=START_ADDRESS;i<=END_ADDRESS;i++){
-            //try{
+            try{
 
                 I2CDevice i2cDevice = i2c.getDevice(i);
                 i2cDevice.write("{'res':'info'}".getBytes());
 
+                byte[] buffer = new byte[5];
+                i2cDevice.read(buffer, 0, 5);
+                String s = Arrays.toString(buffer);
+                ApplicationStartup.getConsole().println(s);
+                ApplicationStartup.getConsole().println(new String(s));
+                things.add(i);
 
-                i2cDevice.read(buffer, 0, 100);
-
-
-            /*}catch(Exception e){
+            }catch(Exception e){
                 ApplicationStartup.getConsole().println("En la direccion "+i+" no hay nada.");
-            }*/
-
-            String s = new String(buffer, "UTF-8");
-            JSONParser parser = new JSONParser();
-            JSONObject json = (JSONObject) parser.parse(s);
-            ApplicationStartup.getConsole().println(json.keySet());
-            things.add(i);
+            }
         }
 
         return things;
